599c44ffbffd07dccab8e02220fce4491fe83281
shah768 <shah.768@osu.edu>
2017-02-06 19:59:11 -0500

    Add files via upload

---
+++ Controllers/ClaimsController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+[Route ("api/claims")]
+
+public class claimsController : Controller
+{
+    //POST api/claims
+    [HttpPost]
+    public IActionResult Post([FromBody]string value)
+    {
+        return Created("", value);
+    }
+
+    // GET api/claims/5
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+    return Ok("The id is: " + id);
+    }
+    // PUT api/claims/id
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromBody]string value)
+    {
+    return NoContent();
+    }
+    // DELETE api/claims/id
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+    return Delete(id);
+    }
+}
